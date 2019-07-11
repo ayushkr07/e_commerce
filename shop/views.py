97de7bfd79ee7da6d60c140ac57e0b0b46b092e4
ayushkr07 <aks010598@gmail.com>
2019-07-11 11:35:35 +0530

    Modified shop's page but error in styling

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'shop/index.html')
+    products=Product.objects.all()
+    n=len(products)
+    nSlides= n//4 + ceil((n/4)-(n//4))
+    stf={'nSlides':nSlides,'products':products,'range':range(1,nSlides)}
+    return render(request,'shop/index.html',stf)
 
 def about(request):
     return HttpResponse("We are at about")
