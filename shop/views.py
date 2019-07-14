45bb5b198a4646d446fb5c2b4e3ee89a9ca020f5
ayushkr07 <aks010598@gmail.com>
2019-07-14 15:47:14 +0530

    Modified shop's homepage and error identified

--- shop/views.py
+++ shop/views.py
@@ -4,10 +4,22 @@ from .models import Product
 from math import ceil
 # Create your views here.
 def index(request):
-    products=Product.objects.all()
-    n=len(products)
-    nSlides= n//4 + ceil((n/4)-(n//4))
-    stf={'nSlides':nSlides,'products':products,'range':range(1,nSlides)}
+    #products=Product.objects.all()
+    #n=len(products)
+    #nSlides= n//4 + ceil((n/4)-(n//4))
+    #allProd=[[products,range(1,nSlides)],
+     #        [products,range(1,nSlides)]]
+    #stf={'allProd':allProd}
+
+    allProd = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        products = Product.objects.filter(category=cat)
+        n = len(products)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allProd.append([products, range(1,nSlides)])
+    stf={'allProd':allProd}
     return render(request,'shop/index.html',stf)
 
 def about(request):
